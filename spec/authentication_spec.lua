d180375d8b3b4d9bf78a5f1e8ca6d6da42fa74ec
Astami <astami@shipperstack.com>
2017-12-02 12:11:26 +0530

    spec based on config.
    Signed-off-by: Astami <astami@shipperstack.com>

--- spec/authentication_spec.lua
+++ spec/authentication_spec.lua
@@ -1,4 +1,8 @@
 expose("an exposed auth", function()
+    local config = require("pl.config")
+    local pretty = require("pl.pretty")
+    _G.arango = config.read('spec/spec.config')
+
     local jsf_post_api_database_open_auth = require("arango.model.jsf_post_api_database_open_auth")
     local new_auth_api = require("arango.auth_api")
     local dkjson = require "dkjson"
@@ -6,8 +10,8 @@ expose("an exposed auth", function()
 
     describe("Testing authentication to arangodb", function()
         it("should able to login", function()
-            local auth = jsf_post_api_database_open_auth.new("root", "openSesame")
-            local auth_api = new_auth_api.new({"http"}, "localhost", 8000)
+            local auth = jsf_post_api_database_open_auth.new(_G.arango.username, _G.arango.password)
+            local auth_api = new_auth_api.new(_G.arango.scheme, _G.arango.host, _G.arango.port)
             local err, http_status, body = auth_api:api_database_open_auth_post(auth)
             local json = dkjson.decode(body);
 
--- spec/database_spec.lua
+++ spec/database_spec.lua
@@ -1,7 +1,18 @@
 describe("Testing database apis to arangodb", function()
-    local inspect = require("inspect")
-    it("should able to create database", function()
+    local pretty = require("pl.pretty")
+    local dkjson = require "dkjson"
+
+    local new_database_api = require("arango.database_api")
+    it("should able to get list of database", function()
+        assert.are_not.equals(_G.arango, nil)
         assert.are_not.equals(_G.auth_token, nil)
-        print(inspect(_G.auth_token))
+        local database_api = new_database_api.new(_G.arango.scheme, _G.arango.host, _G.arango.port)
+        database_api.access_token = _G.auth_token;
+        local err, http_status, body = database_api:api_database_get()
+        assert.are.same(http_status, '200');
+        assert.are.same(err, nil);
+
+        local json = dkjson.decode(body);
+        print(pretty.write(json));
     end)
 end)
