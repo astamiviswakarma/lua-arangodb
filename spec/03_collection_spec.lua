43fd7c79168d4545cea70e24492536ec4d893eb4
Astami <astami@shipperstack.com>
2017-12-04 10:06:53 +0530

    updated create collection.
    Signed-off-by: Astami <astami@shipperstack.com>

--- spec/03_collection_spec.lua
+++ spec/03_collection_spec.lua
@@ -5,8 +5,8 @@ describe("Testing database apis to arangodb", function()
 
     local new_database_api = require("arango.database_api")
     local new_collections_api = require("arango.collections_api")
-    local jsf_get_api_database_new = require("arango.model.jsf_get_api_database_new")
-    local jsf_get_api_database_new_users = require("arango.model.jsf_get_api_database_new_users")
+    local jsf_post_api_collection = require("arango.model.jsf_post_api_collection")
+    local jsf_post_api_collection_opts = require("arango.model.jsf_post_api_collection_opts")
 
     it("should able to get testdb database", function()
         assert.are_not.equals(_G.arango, nil)
@@ -21,6 +21,20 @@ describe("Testing database apis to arangodb", function()
         assert.are.equals(type(json.result), 'table')
         assert.are_not.equals(tablex.find(json.result, "testdb"), nil)
         -- print(pretty.write(json));
+    end)
+
+    it("should able to create collection inside database", function()
+        assert.are_not.equals(_G.arango, nil)
+        assert.are_not.equals(_G.auth_token, nil)
+        local collection_api = new_collections_api.new(_G.arango.scheme, _G.arango.host, _G.arango.port, _G.arango.database)
+        collection_api.access_token = _G.auth_token;
+        local collection_opts = jsf_post_api_collection_opts.new()
+        local collection = jsf_post_api_collection.new(nil, nil, false, false, nil, collection_opts, 'test_coll', nil, nil, nil, 2, nil)
+        local err, http_status, body = collection_api:api_collection_get('true')
+        assert.are.same('200', http_status);
+        assert.are.same(err, nil);
+        local json = dkjson.decode(body);
+        print(pretty.write(json));
     end)
 
     it("should able to get all collections inside database", function()
